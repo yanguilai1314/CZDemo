b99229255e290258ab96aaf23b631e2301445ee0
chenzhi <2414816240@qq.com>
2018-05-24 21:49:33 +0800

    修改

--- MyTest/src/main/java/demo1/Test.java
+++ MyTest/src/main/java/demo1/Test.java
@@ -1,8 +1,15 @@
 package demo1;
 
+import java.util.Date;
+import java.util.Calendar;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello sourcetree");
 		System.out.println("ddddd");
+		
+		Calendar calendar = Calendar.getInstance();
+		Date date = calendar.getTime();
+		
 	}
 }
